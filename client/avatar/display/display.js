6ea8a441e2662fdebf7cefbf1e5a87357354382c
Alain Leon <xerebz@gmail.com>
2014-08-31 00:07:30 -0400

    Added default values for all item types

--- client/avatar/display/display.js
+++ client/avatar/display/display.js
@@ -1,52 +1,68 @@
 Template.avatarDisplay.rendered = function() {
 
-  //remove 'select on double click' bug
+  //remove 'select canvas on double click'
   var canvas = document.getElementById('avatar-canvas');
   canvas.onselectstart = function () { return false; }
 
   //global stage should be file scoped
-  stage = new createjs.Stage("avatar-canvas");
+  stage = new createjs.Stage('avatar-canvas');
 
   //rerun the drawing process if equipment changes
 
   Deps.autorun(function () {
-    var equippedItems = EquippedItems.find({ "userId" : Meteor.userId() });
+    var equippedItems = EquippedItems.find({ 'userId' : Meteor.userId() });
     console.log(equippedItems);
-    console.log("hey");
+    console.log('hey');
     drawItems(equippedItems.fetch());
   });
 
   // autorun for briefs
   // Deps.autorun(function() {
-  //  if (EquippedItems({"user-id": Meteor.userId(), "item.category": "pants"}).count() === 0) {
-  //    UserItems({"user-id": Meteor.userId(), "item.name": "Disappearing Briefs"});
+  //  if (EquippedItems({'user-id': Meteor.userId(), 'item.category': 'pants'}).count() === 0) {
+  //    UserItems({'user-id': Meteor.userId(), 'item.name': 'Disappearing Briefs'});
   //    Meteor.call('handleEquip', this.item);
   //  }
   // });
 
 };
 
+//layer type -> stacking order mapping
+var defaultLayering = {
+  'hair': 124,
+  'makeup': 123,
+  'tights': 122,
+  'mouth': 121,
+  'outerwear': 120,
+  'background': 10,
+  'eyebrows': 120,
+  'eyes': 101,
+  'bgitem': 50,
+  'glasses': 131,
+  'pants': 102,
+  'shoes': 99,
+  'shirt': 104,
+  'skirt': 105,
+  'briefs': 90,
+  'pet': 200,
+  'scarf': 106,
+  'blouse': 107,
+  'stockings': 108,
+  'headphones': 109
+}
+
 var drawItem = function (equippedItem) {
   //draw each layer of the item
   _.each(equippedItem.item.layers, function(itemLayer) {
     if (stage.getChildByName)
     var img = new Image();
-    img.src = "images/"+itemLayer.filename;
+    img.src = 'images/'+itemLayer.filename;
     var bitmap = new createjs.Bitmap(img);
-
-    //layer type -> stacking order mapping
-    var defaultLayeringPriorityTable = {
-      {layerType: 'hair_accessory', stackingPriority: 122},
-      {layerType: 'hair_front', stackingPriority: 120},
-      {layerType: 'shirt', stackingPriority: 114},
-      {layerType: 'bralette', stackingPriority: 113}
-    };
 
     var computedPriority = 0;
 
     //mock-up of layering logic, if it has a layer type, use that, otherwise, use the custom stacking priority 
-    if (itemLayer.layerType) {
-      computedPriority = _.where(defaultLayeringPriorityTable, {layerType:itemLayer.layerType};
+    if (defaultLayering[equippedItem.category]) {
+      computedPriority = defaultLayering[equippedItem.category];
     } else if (itemLayer.zindex) {
       computedPriority = itemLayer.zindex;
     }
